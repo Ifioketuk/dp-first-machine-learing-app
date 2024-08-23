0bd436e707385714889a0f999dbea4b2b9271e0d
Ifioketuk <104146924+Ifioketuk@users.noreply.github.com>
2024-08-23 12:15:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('🤖 CGPA Prediction App')
 
-st.write('')
+st.write('This is an app used to predict the cgpa at the end of your first year based off academic performance at the end of high-school and study habits during the first year semesters.')

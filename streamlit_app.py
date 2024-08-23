b2ad1f975947e0e606725be8d191bfcf5789c6be
Ifioketuk <104146924+Ifioketuk@users.noreply.github.com>
2024-08-23 12:31:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title('🤖 CGPA Prediction App')
 
 st.write('This is an app used to predict the cgpa at the end of your first year based off academic performance at the end of high-school and study habits during the first year semesters.')
-df= pd.read_excel("Nigerian Student's Year One Performance Survey(1-174).xlsx",index_col=0)
+df= pd.read_excel(r"C:\Users\emman\Downloads\Nigerian Student's Year One Performance Survey(1-174).xlsx",index_col=0)
 df

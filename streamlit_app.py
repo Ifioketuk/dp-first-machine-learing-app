19564809379f0e6e6cb368b96e47d2ca8b60e73f
Ifioketuk <104146924+Ifioketuk@users.noreply.github.com>
2024-08-23 13:28:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import requests
 import pandas as pd
-url = "https://github.com/Ifioketuk/dp-first-machine-learing-app/blob/master/Nigerian%20Student's%20Year%20One%20Performance%20Survey(1-174).xlsx"
-
+url = "https://raw.githubusercontent.com/Ifioketuk/dp-first-machine-learing-app/main/Nigerian Student's Year One Performance Survey(1-174).xlsx"
+with open("Nigerian Student's Year One Performance Survey(1-174).xlsx.xlsx","wb") as file:
+      file.write(response.content)
+      
 st.title('🤖 CGPA Prediction App')
 
 st.write('This is an app used to predict the cgpa at the end of your first year based off academic performance at the end of high-school and study habits during the first year semesters.')
 response= requests.get(url)
 
-with open("your_file.py") as file:
-      file.write(response.content)
+
+df= pd.read_excel(r"C:\Users\emman\Downloads\Nigerian Student's Year One Performance Survey(1-174).xlsx",index_col=0)
+st.dataframe(df)

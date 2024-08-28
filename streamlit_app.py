03ac5bd6ded74e4a160552e0b508072b70d6ea5e
Ifioketuk <104146924+Ifioketuk@users.noreply.github.com>
2024-08-28 10:13:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import cross_val_score
+import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import time

9cc2bf4195b1ffba7bd88122c4e7fca3089e85eb
Ifioketuk <104146924+Ifioketuk@users.noreply.github.com>
2024-08-28 17:08:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,100 +101,100 @@ studied_original_course= st.select_slider('Did you study your original course',o
                                                                                         
                                                                                         
 if st.button('Show CGPA'):                                                                                        
-data ={    
-                                                                                        
-    'jamb_score':jamb_score ,
-    'english': english,
-    'maths': maths ,
-    'subject_3': subject_3,
-    'subject_4': subject_4,
-    'subject_5': subject_5,
-    'age_in_year_one': age_in_year_one,
-    'gender':gender ,
-    'has_disability': has_disability,
-    'Did you attend extra tutorials?': ,
-    'extracurricular_participation':,
-    'class_attendance_rating': class_attendance_rating ,
-    'class_participation_rating': class_participation_rating,
-    'used_extra_study_materials': used_extra_study_materials,
-    'morning_study':morning_study ,
-    'afternoon_study':afternoon_study ,
-    'evening_study':evening_study ,
-    'late_night_study':late_night_study ,
-    'days_per_week_reading': days_per_week_reading,
-    'hours_per_day_personal_study': ,
-    'taught_peers': taught_peers,
-    'courses_offered':courses_offered ,
-    'times_fell_sick': ,
-    'study_mode': study_mode,
-    'studied_original_course':studied_original_course ,
-    'What was your monthly allowance in Year One?':allowance ,
-    'teaching_style_rating': teaching_style_rating,
-    'institution_type':institution_type ,
-    'What year did you finish Year One?':year,
-    'grading_system': grading_system}
-
-gpa_data_comp_col= pd.DataFrame(data)
-
-
-grade_features = ['maths', 'english','subject_3', 'subject_4', 'subject_5']
-grade_to_value = {'A': 4, 'B': 3, 'C': 2, 'D': 1, 'F': 0}
-
-# Map grades to values
-for col in grade_features:
-    gpa_data_comp_col[col] = gpa_data_comp_col[col].map(grade_to_value)
-
-# Calculate Combined_grade
-gpa_data_comp_col['Combined_grade'] = (
-    gpa_data_comp_col['english'] +
-    gpa_data_comp_col['maths'] +
-    gpa_data_comp_col['subject_3'] +
-
-    gpa_data_comp_col['subject_4'] +
-    gpa_data_comp_col['subject_5']
-)
-
-# Apply condition and update Combined_grade
-gpa_data_comp_col.loc[
-    (gpa_data_comp_col['maths'] < 1) | (gpa_data_comp_col['english'] < 1),
-    'Combined_grade'
-] = gpa_data_comp_col['Combined_grade'] / 3
-
-bins = [0, 10, 15, 20]  # Define the edges of the bins
-labels = ['Rank 3', 'Rank 2', 'Rank 1']  # Define labels for each bin
-
-# Use pd.cut to create the 'grade_rank' column
-gpa_data_comp_col['grade_rank'] = pd.cut(
-    gpa_data_comp_col['Combined_grade'],
-    bins=bins,
-    labels=labels,
-    right=True,  # Include the right edge of bins
-    include_lowest=True  # Include the lowest edge
-)
-
-
-loaded_kmeans = joblib.load('kmeans_model.pkl')
-gpa_data_comp_col['cluster']= loaded_kmeans.predict(gpa_data_comp_col)
-
-Z = gpa_data_comp_col.drop(['What year did you finish Year One?', 'english', 'maths', 'subject_3', 'subject_4', 'subject_5'], axis=1)  # Features excluding 'id' and 'GPA_normal'
-  # Target variable
-
-# Train-test split
-
-
-categorical_columns =  [col for col in Z.columns if Z[col].dtype in ['object','category']]
-Z[categorical_columns] = Z[categorical_columns].astype(str)
-
-
-ordinal_cod= OrdinalEncoder()
-Z[categorical_columns] = ordinal_cod.fit_transform(Z[categorical_columns])
-
-
-
-model=load("model1.pkl",'rb')
-predictions= model.predict(Z)
-st.write("🤖Your CGPA is")
-st.write(predictions)
+    data ={    
+                                                                                            
+        'jamb_score':jamb_score ,
+        'english': english,
+        'maths': maths ,
+        'subject_3': subject_3,
+        'subject_4': subject_4,
+        'subject_5': subject_5,
+        'age_in_year_one': age_in_year_one,
+        'gender':gender ,
+        'has_disability': has_disability,
+        'Did you attend extra tutorials?': ,
+        'extracurricular_participation':,
+        'class_attendance_rating': class_attendance_rating ,
+        'class_participation_rating': class_participation_rating,
+        'used_extra_study_materials': used_extra_study_materials,
+        'morning_study':morning_study ,
+        'afternoon_study':afternoon_study ,
+        'evening_study':evening_study ,
+        'late_night_study':late_night_study ,
+        'days_per_week_reading': days_per_week_reading,
+        'hours_per_day_personal_study': ,
+        'taught_peers': taught_peers,
+        'courses_offered':courses_offered ,
+        'times_fell_sick': ,
+        'study_mode': study_mode,
+        'studied_original_course':studied_original_course ,
+        'What was your monthly allowance in Year One?':allowance ,
+        'teaching_style_rating': teaching_style_rating,
+        'institution_type':institution_type ,
+        'What year did you finish Year One?':year,
+        'grading_system': grading_system}
+    
+    gpa_data_comp_col= pd.DataFrame(data)
+    
+    
+    grade_features = ['maths', 'english','subject_3', 'subject_4', 'subject_5']
+    grade_to_value = {'A': 4, 'B': 3, 'C': 2, 'D': 1, 'F': 0}
+    
+    # Map grades to values
+    for col in grade_features:
+        gpa_data_comp_col[col] = gpa_data_comp_col[col].map(grade_to_value)
+    
+    # Calculate Combined_grade
+    gpa_data_comp_col['Combined_grade'] = (
+        gpa_data_comp_col['english'] +
+        gpa_data_comp_col['maths'] +
+        gpa_data_comp_col['subject_3'] +
+    
+        gpa_data_comp_col['subject_4'] +
+        gpa_data_comp_col['subject_5']
+    )
+    
+    # Apply condition and update Combined_grade
+    gpa_data_comp_col.loc[
+        (gpa_data_comp_col['maths'] < 1) | (gpa_data_comp_col['english'] < 1),
+        'Combined_grade'
+    ] = gpa_data_comp_col['Combined_grade'] / 3
+    
+    bins = [0, 10, 15, 20]  # Define the edges of the bins
+    labels = ['Rank 3', 'Rank 2', 'Rank 1']  # Define labels for each bin
+    
+    # Use pd.cut to create the 'grade_rank' column
+    gpa_data_comp_col['grade_rank'] = pd.cut(
+        gpa_data_comp_col['Combined_grade'],
+        bins=bins,
+        labels=labels,
+        right=True,  # Include the right edge of bins
+        include_lowest=True  # Include the lowest edge
+    )
+    
+    
+    loaded_kmeans = joblib.load('kmeans_model.pkl')
+    gpa_data_comp_col['cluster']= loaded_kmeans.predict(gpa_data_comp_col)
+    
+    Z = gpa_data_comp_col.drop(['What year did you finish Year One?', 'english', 'maths', 'subject_3', 'subject_4', 'subject_5'], axis=1)  # Features excluding 'id' and 'GPA_normal'
+      # Target variable
+    
+    # Train-test split
+    
+    
+    categorical_columns =  [col for col in Z.columns if Z[col].dtype in ['object','category']]
+    Z[categorical_columns] = Z[categorical_columns].astype(str)
+    
+    
+    ordinal_cod= OrdinalEncoder()
+    Z[categorical_columns] = ordinal_cod.fit_transform(Z[categorical_columns])
+    
+    
+    
+    model=load("model1.pkl",'rb')
+    predictions= model.predict(Z)
+    st.write("🤖Your CGPA is")
+    st.write(predictions)
 
 
 

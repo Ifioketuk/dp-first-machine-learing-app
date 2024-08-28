61982bb733fcf3e1dda3d7860eff5d2860f355df
Ifioketuk <104146924+Ifioketuk@users.noreply.github.com>
2024-08-28 11:40:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,7 @@ bar.empty()
 # Display the final message
 exitstatement=st.empty()
 exitstatement.write('...and now we\'re done!')
+time.sleep(0.001)
 
 #let both statements disappear
 latest_iteration.empty()
@@ -49,3 +50,34 @@ title.empty()
 st.title('🤖 CGPA Prediction App')
 st.write('This is an app used to predict the CGPA at the end of your first year based on academic performance at the end of high school and study habits during the first year semesters.')
 
+st.write("Fill in the following features to get your possible GPA")
+col1, col2 = st.columns(2)
+with col1:
+    st.text_input("age_in_year_one")
+    st.text_input("gender")
+    st.text_input('has_disability')
+    st.text_input('times_fell_sick')
+    st.text_input("institution_type")
+    st.text_input('What year did you finish Year One')
+with col2:
+    st.text_input("Enter your JAMB score(1-400)")
+    st.write("GRADES(A-F) in the following:")
+    st.text_input('english')
+    st.text_input('maths')
+    st.text_input('subject_3')
+    st.text_input('subject_4')
+    st.text_input('subject_5')
+st.write("Give your ratings in the following")
+class_participation_rating = st.radio("class_attendance_rating",('⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'))
+teaching_style_rating = st.radio("teaching_style_rating",('⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'))
+extracurricular_participation = st.radio("extracurricular_participation",('⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'))
+morning_study = st.radio("morning_study",('⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'))
+afternoon_study = st.radio("afternoon_study",('⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'))
+evening_study = st.radio("evening_study",('⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'))
+
+st.text_input("days_per_week_reading(1-7)")
+st.text_input("monthly allowance in Year One")
+
+
+
+    
